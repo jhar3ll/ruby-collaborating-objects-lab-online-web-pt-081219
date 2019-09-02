9001009a0d3cfe0ebc315f3a4bedde82ae05d60a
Jharell Chambers <jharell.chambers@selu.edu>
2019-09-02 03:17:22 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -23,10 +23,6 @@ class Artist
 
   def self.find_or_create_by_name(name)
     self.find(name) ? self.find(name) : self.new(name)
-    if name 
-     self.find(name)  
-     else 
-     self.new(name)
   end
 
   def self.find(name)
